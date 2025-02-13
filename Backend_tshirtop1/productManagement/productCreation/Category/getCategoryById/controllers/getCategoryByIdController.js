fe54554c76d5c05ddca109e53b16fcfcbc6bd0e7
dcaguilarc <dcaguilarc@uce.edu.ec>
2025-02-13 15:40:19 -0500

    Update getCategoryByIdController.js

--- Backend_tshirtop1/productManagement/productCreation/Category/getCategoryById/controllers/getCategoryByIdController.js
+++ Backend_tshirtop1/productManagement/productCreation/Category/getCategoryById/controllers/getCategoryByIdController.js
@@ -5,20 +5,20 @@ const getCategoryById = async (req, res) => {
     const categoryId =  parseInt(req.params.id, 10);
 
     if (isNaN(categoryId)) {
-      return res.status(400).json({ error: 'Invalid category ID' });
+      return res.status(400).json({ error: 'Invalid category ID.' });
     }
     console.log(`🔍 Searching for category with ID: ${categoryId}`);
     const category = await Category.findByPk(categoryId); // Find category by primary key
     console.log('✅ Category found:', category);
 
     if (!category) {
-      return res.status(404).json({ error: 'Category not found' });
+      return res.status(404).json({ error: 'Category not found.' });
     }
 
     return res.status(200).json(category);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Something went wrong' });
+    return res.status(500).json({ error: 'Something went wrong.' });
   }
 };
 
