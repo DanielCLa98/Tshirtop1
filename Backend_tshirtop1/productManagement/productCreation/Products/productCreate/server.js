b2b7d91328d1b3de7f248839e1ec9f8713f4b787
Daniel Clavijo <156672995+DanielCLa98@users.noreply.github.com>
2025-02-16 16:10:37 -0500

    Update server.js

--- Backend_tshirtop1/productManagement/productCreation/Products/productCreate/server.js
+++ Backend_tshirtop1/productManagement/productCreation/Products/productCreate/server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const sequelize = require('./config/database');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
-
+//tst
 const app = express();
 const port = process.env.PORT || 3000;
 
