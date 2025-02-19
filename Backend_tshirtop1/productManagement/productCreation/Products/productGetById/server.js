8387da0c41484ffac62d0ff675a66023b642fbd4
dcaguilarc <dcaguilarc@uce.edu.ec>
2025-02-19 16:36:24 -0500

    Update server.js

--- Backend_tshirtop1/productManagement/productCreation/Products/productGetById/server.js
+++ Backend_tshirtop1/productManagement/productCreation/Products/productGetById/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
-
+//test
 const app = express();
 const PORT = process.env.PORT || 3002;
 //test
