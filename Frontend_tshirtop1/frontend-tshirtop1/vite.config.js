2857b8c5c982620fb0ccd9f8bdc20c3f0ded2a9a
Daniel Clavijo <daniel-cla@hotmail.com>
2025-02-19 13:28:56 -0500

    feat: :sparkles: new config

--- Frontend_tshirtop1/frontend-tshirtop1/vite.config.js
+++ Frontend_tshirtop1/frontend-tshirtop1/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-//test
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    preview: {
+      allowedHosts: ['tshirtop-1262192193.us-east-1.elb.amazonaws.com'], // Agregar el host aquí
+    },
+  },
 })
